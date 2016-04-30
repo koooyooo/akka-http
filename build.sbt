584017ba33f06ec18b57b3b83a2ff0abcddcde82
koooyooo <jab.cross.hook@gmail.com>
2016-04-30 12:24:20 +0900

    一旦無事に動いたのでコミット

--- build.sbt
+++ build.sbt
@@ -5,14 +5,13 @@ lazy val commonSettings = Seq (
   scalaVersion := "2.11.8"
 )
 
-lazy val root =
+lazy val root1 =
   (project in file(".")).
     settings(commonSettings: _*).
     settings(
       name := "akka-http",
       scalacOptions += "-deprecation",
       libraryDependencies ++= Seq (
-//        "com.typesafe.akka" %% "akka-http-core" % "2.4.3",
         "com.typesafe.akka" %% "akka-stream" % "2.4.3",
         "com.typesafe.akka" %% "akka-http-experimental" % "2.4.3"
       )
--- src/main/scala/Sample.scala
+++ src/main/scala/Sample.scala
@@ -1,6 +1,6 @@
 package main.scala
 
-import akka.stream.scaladsl.{Flow, GraphDSL, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Sink, Source}
 import akka._
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, ClosedShape, FlowShape}
@@ -16,12 +16,17 @@ object Sample {
     val graph = GraphDSL.create() {implicit builder =>
       import GraphDSL.Implicits._
 
-      val flw1 = builder.add(Flow[Int].map{x: Int => x * 10})
-      val flw2 = builder.add(Flow[Int].map{x: Int => println(x); x + 1 })
+      val flw1 = builder.add(Flow[Int].map{x: Int => x })
+      val flw2 = builder.add(Flow[Int].map{x: Int => x * 10  })
+      val flw3 = builder.add(Flow[Int].map{x: Int => x * 100 })
+      val flw4 = builder.add(Flow[Int].map{x: Int => x })
+      val bc = builder.add(Broadcast[Int](2))
+      val mg = builder.add(Merge[Int](2))
 
-      flw1 ~> flw2
+      flw1 ~> bc ~> flw2 ~> mg ~> flw4
+              bc ~> flw3 ~> mg
 
-      FlowShape[Int, Int](flw1.in, flw2.out)
+      FlowShape[Int, Int](flw1.in, flw4.out)
     }
 
     Source(1 to 5).via(graph).to(Sink.foreach(println(_))).run()
